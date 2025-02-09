a49925049de7fb40076bf62fc75829b00ed79ebf
z farahi <farahi.zubair121@gmail.com>
2025-02-09 15:01:14 -0500

    custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        (
+            'Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        ('Important dates', {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 
+                'password1', 
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',)
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
